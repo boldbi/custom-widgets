644012561feaf25764414a2afc9d07e2e83278de
Sathishkanniyappan <92366726+Sathishkanniyappan@users.noreply.github.com>
2023-11-27 20:00:11 +0530

    updated changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,43 @@ import gulp from 'gulp';
 import zip from 'gulp-zip';
 import path from 'path';
 import fs from 'fs';
+import rename from 'gulp-rename';
+import replace from 'gulp-replace';
+import { v4 as uuidv4 } from 'uuid';
+import jsonEditor from 'gulp-json-editor';
+
+gulp.task('create-customwidget', (done) => {
+	const currentDir = process.cwd();
+	const tempFilePath = path.join(currentDir, 'TemplateFiles');
+	const widgetName = (process.env.Widget_Name).trim();
+	
+	const srcFilePath = path.join(currentDir,'TemplateFiles/src/sourcefile.js');
+	const configFilePath = path.join(currentDir,'TemplateFiles/widgetconfig.json');
+	const randomGuid = uuidv4();
+
+	gulp.src(`${tempFilePath}/**/*`)
+		.pipe(rename((file) => {
+			file.dirname = file.dirname.replace('TemplateFiles', widgetName);
+		}))
+		.pipe(gulp.dest(path.join(currentDir, widgetName)))
+		.on('end', function () {
+			gulp.src(srcFilePath)
+				.pipe(replace(/guid: ([\"]).*([\"])/g, 'guid: "' + randomGuid + '"'))
+				.pipe(replace(/widgetName: ([\"]).*([\"])/g, 'widgetName: "' + widgetName + '"'))
+				.pipe(gulp.dest(path.join(currentDir, widgetName, 'src')))
+				.on('end', function () {
+					const keyValuePairs = {
+						guid: randomGuid,
+						widgetName: widgetName,
+						displayName: widgetName
+					};
+					gulp.src(configFilePath)
+						.pipe(jsonEditor(keyValuePairs))
+						.pipe(gulp.dest(path.join(currentDir, widgetName)))
+						.on('end', function(){done();});
+				});
+		});
+});
 
 gulp.task('pack-all-customwidgets', (done) => {
   const currentDir = process.cwd();
@@ -49,5 +86,5 @@ gulp.task('pack-customwidget', (done) => {
 	done();
 });
 
-gulp.task('run-tasks', gulp.series('pack-all-customwidgets', 'pack-customwidget'));  
+gulp.task('run-tasks', gulp.series('pack-all-customwidgets','pack-customwidget','create-customwidget'));  
 
